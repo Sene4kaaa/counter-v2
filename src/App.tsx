3e0bfa1d9cd4724188479a49b02ea00c7380a1de
Sene4kaaa <senkevich92@mail.ru>
2025-01-27 10:47:40 +0300

    Add Settings button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import {useState} from 'react'
 import './App.css'
 import Count from "./components/Count/Count.tsx";
+import Settings from "./components/Settings/Settings.tsx";
+import Button from "./components/Button/Button.tsx";
 
 function App() {
     const [count, setCount] = useState(0)
+    const [settings, setSettings] = useState(false)
+
+
+    const onClickSettingsOn = () => {
+        setSettings(!settings)
+    }
 
     return (
         <>
-            <Count count={count} setCount={setCount}/>
+            {
+                settings ? <Settings/>
+                    : <Count count={count} setCount={setCount}/>}
+            <Button name={'Settings'} callback={onClickSettingsOn}/>
+
         </>
     )
 }
---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,11 @@
+
+
+const Settings = () => {
+    return (
+        <div>
+            <input/>
+        </div>
+    );
+};
+
+export default Settings;
